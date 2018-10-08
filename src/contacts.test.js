8cab7571ded7044db77d2374d8ba71f1fffdd82c
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-08 16:47:57 -0500

    completed testing of zipCodes

--- src/contacts.test.js
+++ src/contacts.test.js
@@ -1,6 +1,10 @@
 import { groupTest } from './testlib'
 import * as regex from './index'
 import { goodUsPhones, badUsPhones } from './test-data/usPhones'
+import { goodZipCodes, badZipCodes } from './test-data/zipCodes'
 
 groupTest(goodUsPhones, regex.usPhone, true)
 groupTest(badUsPhones, regex.usPhone, false)
+
+groupTest(goodZipCodes, regex.zipCode, true)
+groupTest(badZipCodes, regex.zipCode, false)
