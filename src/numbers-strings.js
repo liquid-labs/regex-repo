e0d3d8ac94243a401c3d43b7e55e8559a8f71147
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-09 09:58:04 -0500

    added zeroTo360 and zeroTo360Float in prep for HSL recognizer

--- src/numbers-strings.js
+++ src/numbers-strings.js
@@ -1,9 +1,12 @@
 // Note, this module is used internally, so these aStr exported to for other
 // modules to use, but the file is not Str-exported through index.
-export const zeroTo255Str = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
 export const zeroTo1FloatStr = '(0|0?\\.[0-9]+|1(\\.0+)?)'
 export const zeroTo100PercentStr = '([0-9]|[1-9][0-9]|100)\\%'
 export const zeroTo100FloatPercentStr =
   '(([0-9]|[1-9][0-9])(\\.[0-9]+)?|100(\\.0+)?)\\%'
+export const zeroTo255Str = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
 export const zeroTo255FloatStr =
   '(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])(\\.[0-9]+)?|255(\\.0+)?)'
+export const zeroTo360Str = '([0-9]|[1-9][0-9]|[1-2][0-9][0-9]|3[0-5][0-9]|360)'
+export const zeroTo360FloatStr =
+  '(([0-9]|[1-9][0-9]|[1-2][0-9][0-9]|3[0-5][0-9])(\\.[0-9]+)?|360(\\.0+)?)'
--- src/numbers.test.js
+++ src/numbers.test.js
@@ -10,28 +10,34 @@ const notZeroTo100Float = [-1, -99, 100.001, 101, 'a']
 const notZeroTo100 = notZeroTo100Float.concat([0.1, '10.0'])
 const zeroTo255 = zeroTo100.concat([101, 200, 254, 255])
 const zeroTo255Float = zeroTo255.concat(['0.0', 0.1, 1.2, 254.98765, '255.0'])
+const zeroTo360 = zeroTo255.concat(300, 359, 360)
+const zeroTo360Float = zeroTo360.concat('0.0', 0.1, '359.0', 359.1, 360.0)
 
 groupTest(regex.zeroTo1Float,
   stringify([0, 0.0, 0.1, 0.54321, 0.9, 0.99999, 1, 1.0]),
   stringify([-1.1, -1, -0.1, 1.001, 2]),
   'zeroTo1Float')
-
 groupTest(regex.zeroTo100Percent,
   stringifyPerc(zeroTo100),
   stringifyPerc(notZeroTo100),
   'zeroTo100Percent')
-
 groupTest(regex.zeroTo100FloatPercent,
   stringifyPerc(zeroTo100Float),
   stringifyPerc(notZeroTo100Float),
   'zeroTo100FloatPercent')
-
 groupTest(regex.zeroTo255,
   stringify(zeroTo255),
   stringify([-1, 256, 1000, 0.1]),
   'zeroTo255')
-
 groupTest(regex.zeroTo255Float,
   stringify(zeroTo255Float),
   stringify([-1, 256, 1000, 255.1]),
   'zeroTo255Float')
+groupTest(regex.zeroTo360,
+  stringify(zeroTo360),
+  stringify([-1, 361, 1000, 0.1]),
+  'zeroTo360')
+groupTest(regex.zeroTo360Float,
+  stringify(zeroTo360Float),
+  stringify([-1, 361, 1000, 360.1]),
+  'zeroTo360Float')
