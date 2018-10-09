123cf34a579d4cf48e77ab8f6d20836ecd6410b0
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-08 22:27:38 -0500

    reworked rgb1 and rgba1 to be separate; added support for percent notation

--- src/css.js
+++ src/css.js
@@ -15,9 +15,16 @@ const cssPreColorsRe = '^(' + Object.keys(pre.cssPreColors).join('|') + ')$'
 export const cssPreColors = new RegExp(cssPreColorsRe, 'i')
 
 const zeroTo255Re = '([1-9]|[1-9][0-9]|1[0-9][0-9]|25[0-5]|2[0-4][0-9])'
-const rgb1Re = `rgb\\((\\s*${zeroTo255Re}\\s*,){2}\\s*${zeroTo255Re}\\s*\\)`
-const rgba1Re = `rgba\\((\\s*${zeroTo255Re}\\s*,){3}\\s*${zeroTo255Re}\\s*\\)`
-const rgbBoth1Re = `^(${rgb1Re}|${rgba1Re})$`
-export const rgb1 = new RegExp(rgbBoth1Re)
+const zeroTo1Re = '(1(\\.0+)?|0?\\.[0-9]+)'
+const percentRe = '([0-9]|[1-9][0-9]|100)\\%'
+const alphaRe = `(${zeroTo1Re}|${percentRe})`
+const rgb1IntRe = `rgb\\((\\s*${zeroTo255Re}\\s*,){2}\\s*${zeroTo255Re}\\s*\\)`
+const rgb1PercRe = `rgb\\((\\s*${percentRe}\\s*,){2}\\s*${percentRe}\\s*\\)`
+const rgb1Re = `(${rgb1IntRe}|${rgb1PercRe})`
+const rgba1IntRe = `rgba\\((\\s*${zeroTo255Re}\\s*,){3}\\s*${alphaRe}\\s*\\)`
+const rgba1PercRe = `rgba\\((\\s*${percentRe}\\s*,){3}\\s*${alphaRe}\\s*\\)`
+const rgba1Re = `(${rgba1IntRe}|${rgba1PercRe})`
+export const rgb1 = new RegExp(rgb1Re)
+export const rgba1 = new RegExp(rgba1Re)
 // In level 4, rgba is an alias for rgb
 export const rgb = new RegExp(`^rgba?\\((\\s*${zeroTo255Re}\\s*,){2,3}\\s*${zeroTo255Re}\\s*\\)$`)
--- src/css.test.js
+++ src/css.test.js
@@ -1,7 +1,7 @@
 import { groupTest } from './testlib'
 import * as regex from './index'
 import { validHexColors1, validHexColors, invalidHexColors } from './test-data/hexColors'
-import { validRgb1, invalidRgb1, validRgb, invalidRgb } from './test-data/rgbColors'
+import { validRgb1, validRgba1, invalidRgb1, validRgb, invalidRgb } from './test-data/rgbColors'
 
 groupTest(regex.hexColor1, validHexColors1, invalidHexColors)
 groupTest(regex.hexColor, validHexColors, invalidHexColors)
@@ -36,4 +36,5 @@ test('cssPreColors fails to match invalid', () => {
 })
 
 groupTest(regex.rgb1, validRgb1, invalidRgb1)
-groupTest(regex.rgb, validRgb, invalidRgb)
+groupTest(regex.rgba1, validRgba1, invalidRgb1) // note, same invalid group
+//groupTest(regex.rgb, validRgb, invalidRgb)
