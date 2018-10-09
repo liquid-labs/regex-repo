ed7a5a4c1b958e255e4c6abc17ac1e30a959db41
Zane Rockenbaugh <zane@liquid-labs.com>
2018-10-09 08:59:57 -0500

    refactored css.js to use lockdown consistently; pulled out zeroTo255FloatStr

--- src/css.js
+++ src/css.js
@@ -1,6 +1,6 @@
 import * as pre from './css-color-data'
 import { lockdownRe } from './lib'
-import { zeroTo255Str, zeroTo1FloatStr, percentStr } from './numbers-strings'
+import { zeroTo255Str, zeroTo1FloatStr, percentStr, zeroTo255FloatStr } from './numbers-strings'
 
 // supports latest rgba hex
 const hexColorStr = '#([a-f0-9]{6}|[a-f0-9]{8}|[a-f0-9]{3,4})'
@@ -15,18 +15,15 @@ export const cssPreColors = lockdownRe(Object.keys(pre.cssPreColors), 'i')
 const alphaStr = `(${zeroTo1FloatStr}|${percentStr})`
 const rgb1IntStr = `rgb\\((\\s*${zeroTo255Str}\\s*,){2}\\s*${zeroTo255Str}\\s*\\)`
 const rgb1PercStr = `rgb\\((\\s*${percentStr}\\s*,){2}\\s*${percentStr}\\s*\\)`
-const rgb1Str = `(${rgb1IntStr}|${rgb1PercStr})`
 const rgba1IntStr = `rgba\\((\\s*${zeroTo255Str}\\s*,){3}\\s*${alphaStr}\\s*\\)`
 const rgba1PercStr = `rgba\\((\\s*${percentStr}\\s*,){3}\\s*${alphaStr}\\s*\\)`
-const rgba1Str = `(${rgba1IntStr}|${rgba1PercStr})`
-export const rgb1 = new RegExp(rgb1Str)
-export const rgba1 = new RegExp(rgba1Str)
+export const rgb1 = lockdownRe([rgb1IntStr, rgb1PercStr])
+export const rgba1 = lockdownRe([rgba1IntStr, rgba1PercStr])
 // In level 4, rgba is an alias for rgb, supports floats, and space notation
 // NOTE: The spec allows float values like '+.25e2%', which cannot be recognized
 // via RE and are not supported.
-const zeroTo255FloatStr = '(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])(\\.[0-9]+)?|255(\\.0+)?)'
 const rgbDecFuncStr = `(\\s*${zeroTo255FloatStr}\\s*,){2}\\s*${zeroTo255FloatStr}\\s*(,\\s*${alphaStr}\\s*)?`
 const rgbPercFuncStr = `(\\s*${percentStr}\\s*,){2}\\s*${percentStr}\\s*(,\\s*${alphaStr}\\s*)?`
 const rgbDecSpaceStr = `(\\s*${zeroTo255FloatStr}\\s+){2}${zeroTo255FloatStr}\\s*(/\\s*${alphaStr}\\s*)?`
 const rgbPercSpaceStr = `(\\s*${percentStr}\\s+){2}${percentStr}\\s*(/\\s*${alphaStr}\\s*)?`
-export const rgb = new RegExp(`^rgba?\\((${rgbDecFuncStr}|${rgbPercFuncStr}|${rgbDecSpaceStr}|${rgbPercSpaceStr})\\s*\\)$`)
+export const rgb = lockdownRe(`rgba?\\((${rgbDecFuncStr}|${rgbPercFuncStr}|${rgbDecSpaceStr}|${rgbPercSpaceStr})\\s*\\)`)
--- src/numbers-strings.js
+++ src/numbers-strings.js
@@ -3,3 +3,5 @@
 export const zeroTo255Str = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
 export const zeroTo1FloatStr = '(0?\\.[0-9]+|1(\\.0+)?)'
 export const percentStr = '([0-9]|[1-9][0-9]|100)\\%'
+export const zeroTo255FloatStr =
+  '(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])(\\.[0-9]+)?|255(\\.0+)?)'
