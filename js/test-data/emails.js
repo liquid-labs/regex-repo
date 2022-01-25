bbd74e6f21b13c47561972a73d0a735867da881c
Zane Rockenbaugh <zane@liquid-labs.com>
2022-01-25 11:18:27 -0600

    update emals for long TLDs; add long TLD tests

--- js/test-data/emails.js
+++ js/test-data/emails.js
@@ -3,13 +3,15 @@ export const goodEmails = [
   'foo@bar.org',
   'foo@bar.xqc',
   'foo@baz.bar.xqc',
-  'foo-_18+Z.t%c@Bar-teg38w.co'
+  'foo-_18+Z.t%c@Bar-teg38w.co',
+  'foo@some.reallylongtld'
 ]
 
 export const badEmails = [
   'foo.bar.com',
   'foo@bar.c',
-  'foo@.com'
+  'foo@.com',
+  'foo@some.really-long-tld-with-dashes'
 ]
 const badDomainChars = ['_', '@', '+', '%']
 badDomainChars.forEach((c) => badEmails.push(`foo@bar${c}baz.com`))
