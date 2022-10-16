6aca66ab06193fbf37e9ad453bb487ab04868ae3
Zane Rockenbaugh <zane@liquid-labs.com>
2022-10-16 12:40:25 -0500

    partial support for exporting RE strings as well as REs

--- js/contacts.js
+++ js/contacts.js
@@ -1,2 +1,4 @@
-export const usPhone = /^(\+?1[._ -]?)?(\(\d{3}\)|\d{3})[._ -]?\d{3}[._ -]?\d{4}$/
-export const zipCode = /^\d{5}([._ -]?\d{4})?$/
+export const usPhoneReString = '^(\\+?1[._ -]?)?(\\(\\d{3}\\)|\\d{3})[._ -]?\\d{3}[._ -]?\\d{4}$'
+export const usPhone = new RegExp(usPhoneReString)
+export const zipCodeReString = '^\\d{5}([._ -]?\\d{4})?$'
+export const zipCode = new RegExp(zipCodeReString)
--- js/web.js
+++ js/web.js
@@ -1,19 +1,21 @@
 // Base RE cribbed from: https://github.com/chriso/validator.js via https://stackoverflow.com/a/22648406/929494
 // Annotations cribbed from https://gist.github.com/dperini/729294
+export const urlReString = '^'
+  // protocol ID
+  + '(?!mailto:)(?:(?:http|https|ftp)://)'
+  // user + pass
+  + '(?:\\S+(?::\\S*)?@)?'
+  // IP address dotted notation octets
+// excludes loopback network 0.0.0.0
+// excludes reserved space >= 224.0.0.0
+// excludes network & broacast addresses
+// (first & last IP address of each class)
+  + '(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])'
+  + '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}'
+  + '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$'
 export const url =
-  new RegExp('^'
-    // protocol ID
-    + '(?!mailto:)(?:(?:http|https|ftp)://)'
-    // user + pass
-    + '(?:\\S+(?::\\S*)?@)?'
-    // IP address dotted notation octets
-  // excludes loopback network 0.0.0.0
-  // excludes reserved space >= 224.0.0.0
-  // excludes network & broacast addresses
-  // (first & last IP address of each class)
-    + '(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])'
-    + '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}'
-    + '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i')
+  new RegExp(urlReString, 'i')
 
 // TODO: properly support all domain names https://github.com/liquid-labs/regex-repo/issues/3
-export const email = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+export const emailReString = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$'
+export const email = new RegExp(emailReString, 'i')
