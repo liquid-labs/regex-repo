fcbfd923673d00a3dedcd984aad3e303b52d5f40
Zane Rockenbaugh <zane@liquid-labs.com>
2023-09-05 08:57:36 -0500

    added license preamble

--- src/contacts.js
+++ src/contacts.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 Liquid Labs LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 export const usPhoneReString = '^(\\+?1[._ -]?)?(\\(\\d{3}\\)|\\d{3})[._ -]?\\d{3}[._ -]?\\d{4}$'
 export const usPhone = new RegExp(usPhoneReString)
 export const zipCodeReString = '^\\d{5}([._ -]?\\d{4})?$'
