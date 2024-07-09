6e2abc834f13dc1eb6788c903cb0a7e60360c274
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-09 16:28:26 -0500

    Update S3 bucket REs for all forbidden prefix and suffix

--- src/aws.js
+++ src/aws.js
@@ -13,13 +13,16 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import { ipFormatREString } from './web'
 
-// credit to: https://stackoverflow.com/a/50484916/929494
-export const awsS3TABucketNameREString = '(?!(^xn--|.+-s3alias$))^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$'
-// AWS: Matches S3 Transfer Acceleration compatible S3 bucket name. Note `awsS3TABucketNameREString` cannot be used for partial matches.
+const invalidS3Partials = `(?!^${ipFormatREString}|^xn--|^sthree-|.+-s3alias$|.+--ol-s3$)`
+// const invalidS3Partials = `(?!^${ipFormatREString}|^xn--|^sthree-)`
+
+export const awsS3TABucketNameREString = invalidS3Partials + '^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$'
+// AWS: Matches (most) S3 Transfer Acceleration compatible S3 bucket name. Note `awsS3TABucketNameREString` cannot be used for partial matches.
 export const awsS3TABucketNameRE = new RegExp(awsS3TABucketNameREString)
 
-// credit to: https://stackoverflow.com/a/50484916/929494
-export const awsS3BucketNameREString = '(?!(^((2(5[0-5]|[0-4][0-9])|[01]?[0-9]{1,2})\\.){3}(2(5[0-5]|[0-4][0-9])|[01]?[0-9]{1,2})$|^xn--|.+-s3alias$))^[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$'
-// AWS: Matches valid S3 bucket name. Note `awsS3BucketNameREString` cannot be used for partial matches.
+export const awsS3BucketNameREString = invalidS3Partials + '^[a-z0-9](?:\\.?[a-z0-9-]+)+[a-z0-9]$'
+// export const awsS3BucketNameREString = '^[a-z0-9](?:\\.?[a-z0-9-]+)+[a-z0-9]$'
+// AWS: Matches (most) valid S3 bucket name. Note `awsS3BucketNameREString` cannot be used for partial matches. Does not enforce 63 character limit.
 export const awsS3BucketNameRE = new RegExp(awsS3BucketNameREString)
