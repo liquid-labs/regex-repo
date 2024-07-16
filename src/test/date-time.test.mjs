837f10303737005c364cac89234bb5b89749af1c
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-16 13:36:38 -0500

    test matching groups for rfc2822RE

--- src/test/date-time.test.mjs
+++ src/test/date-time.test.mjs
@@ -32,24 +32,39 @@ groupTestPartial("'" + regex.iso8601DateREString + "'", valid8601Dates, invalid8
   ['2024027T12:30.168+1000', '2024', undefined, undefined, undefined, undefined, '027', undefined, '12', undefined, '30', '168', undefined, undefined, '+1000'],
 ].forEach(([input, year, month, day, week, dayOfWeek, ordinalDate, endOfDay, hour, hourFrac, minutes, minFrac, seconds, secFrac, timezone]) => {
     const match = input.match(regex.iso8601DateRE)
-    test('matches year', () => expect(match[1]).toBe(year))
-    test('matches month', () => expect(match[3]).toBe(month))
-    test('matches day', () => expect(match[4]).toBe(day))
-    test('matches week', () => expect(match[5]).toBe(week))
-    test('matches day of week', () => expect(match[6]).toBe(dayOfWeek))
-    test('matches ordinal date', () => expect(match[7]).toBe(ordinalDate))
-    test('matches end of day', () => expect(match[8]).toBe(endOfDay))
-    test('matches hour', () => expect(match[10]).toBe(hour))
-    test('matches decimal hour', () => expect(match[11]).toBe(hourFrac))
-    test('matches minutes', () => expect(match[13]).toBe(minutes))
-    test('matches decimal minutes', () => expect(match[14]).toBe(minFrac))
-    test('matches seconds', () => expect(match[15]).toBe(seconds))
-    test('matches decimal seconds', () => expect(match[16]).toBe(secFrac))
-    test('matches time zone', () => expect(match[17]).toBe(timezone))  
+    test('iso8601RE matches year', () => expect(match[1]).toBe(year))
+    test('iso8601RE matches month', () => expect(match[3]).toBe(month))
+    test('iso8601RE matches day', () => expect(match[4]).toBe(day))
+    test('iso8601RE matches week', () => expect(match[5]).toBe(week))
+    test('iso8601RE matches day of week', () => expect(match[6]).toBe(dayOfWeek))
+    test('iso8601RE matches ordinal date', () => expect(match[7]).toBe(ordinalDate))
+    test('iso8601RE matches end of day', () => expect(match[8]).toBe(endOfDay))
+    test('iso8601RE matches hour', () => expect(match[10]).toBe(hour))
+    test('iso8601RE matches decimal hour', () => expect(match[11]).toBe(hourFrac))
+    test('iso8601RE matches minutes', () => expect(match[13]).toBe(minutes))
+    test('iso8601RE matches decimal minutes', () => expect(match[14]).toBe(minFrac))
+    test('iso8601RE matches seconds', () => expect(match[15]).toBe(seconds))
+    test('iso8601RE matches decimal seconds', () => expect(match[16]).toBe(secFrac))
+    test('iso8601RE matches time zone', () => expect(match[17]).toBe(timezone))  
 })
 
 groupTest(regex.iso8601DateTimeRE, valid8601DateTimes, invalid8601DateTimes, 'ISO 8601 date with time')
 groupTestPartial(regex.iso8601DateTimeREString, valid8601DateTimes, invalid8601DateTimes, 'ISO 8601 date with time')
 
 groupTest(regex.rfc2822DateRE, validRFC2822Dates, invalidRFC2822Dates, 'RFC 2822 dates')
-groupTestPartial("'" + regex.rfc2822DateREString + "'", validRFC2822Dates, invalidRFC2822Dates, 'RFC 2822 dates', "Hi '", "' there")
+groupTestPartial("'" + regex.rfc2822DateREString + "'", validRFC2822Dates, invalidRFC2822Dates, 'RFC 2822 dates', "Hi '", "' there");
+
+[
+  ['Mon, 1 Jan 2024 12:30:40 +1000', 'Mon', '1', 'Jan', '2024', '12', '30', '40', '+1000'],
+  ['1 Jan 2024 12:30:40 +1000', undefined, '1', 'Jan', '2024', '12', '30', '40', '+1000']
+].forEach(([input, day, dayOfMonth, month, year, hour, minutes, seconds, timezone]) => {
+  const match = input.match(regex.rfc2822DateRE)
+  test('rfc2822RE matches day', () => expect(match[1]).toBe(day))
+  test('rfc2822RE matches day of month', () => expect(match[2]).toBe(dayOfMonth))
+  test('rfc2822RE matches month', () => expect(match[3]).toBe(month))
+  test('rfc2822RE matches year', () => expect(match[4]).toBe(year))
+  test('rfc2822RE matches hour', () => expect(match[5]).toBe(hour))
+  test('rfc2822RE matches minutes', () => expect(match[6]).toBe(minutes))
+  test('rfc2822RE matches seconds', () => expect(match[7]).toBe(seconds))
+  test('rfc2822RE matches timezone', () => expect(match[8]).toBe(timezone))
+})
