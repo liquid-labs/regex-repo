35c15cef74feea31e1956b2153751a19e4d6d26d
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-19 13:18:56 -0500

    lint formatting

--- src/test/data/rfc-2822-date-times.mjs
+++ src/test/data/rfc-2822-date-times.mjs
@@ -55,7 +55,7 @@ export const invalidRFC2822Times = [
 export const validRFC2822Dates = [
   'Mon, 7 Jan 1992 12:12',
   '6 Jan 1992 12:12 UT',
-  '6 Jan 1992 12:13:14 UT',
+  '6 Jan 1992 12:13:14 UT'
 ]
 
 export const invalidRFC2822Dates = [
